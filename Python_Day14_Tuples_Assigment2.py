7b0506370af4b4523947defe7b5d36aa4661b128
akash-singh-05 <141124568+akash-singh-05@users.noreply.github.com>
2024-09-12 12:00:56 +0530

    Add files via upload

---
+++ Python_Day14_Tuples_Assigment2.py
@@ -0,0 +1,13 @@
+'''Write a Python program to convert a list to a tuple.
+ Input: listx = [5, 10, 7, 4, 15, 3] Output: (5, 10, 7, 4, 15, 3)'''
+
+# Define the list
+listx = [5, 10, 7, 4, 15, 3]
+
+# Convert the list to a tuple using tuple() function
+tuplex = tuple(listx)
+
+# Print the tuple
+print("Converted tuple:", tuplex)
+
+#Output:-Converted tuple: (5, 10, 7, 4, 15, 3)
