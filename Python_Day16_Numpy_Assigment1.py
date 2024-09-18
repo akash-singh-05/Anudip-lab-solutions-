73205cabe07571d4e22d095d5c0dd76244937b88
akash-singh-05 <141124568+akash-singh-05@users.noreply.github.com>
2024-09-18 12:09:21 +0530

    Add files via upload

---
+++ Python_Day16_Numpy_Assigment1.py
@@ -0,0 +1,16 @@
+'''Convert the below list into numpy array then display the array
+ Input: my_list = [1, 2, 3, 4, 5]'''
+
+import numpy as np
+
+# Input list
+my_list = [1, 2, 3, 4, 5]
+
+# Convert list to NumPy array
+my_array = np.array(my_list)
+
+# Display the array
+print(my_array)
+
+#Output:- [1 2 3 4 5]
+
