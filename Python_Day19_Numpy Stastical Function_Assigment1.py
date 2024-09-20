637493c52ea0044cc9237a3ebcb33353425dbb6c
akash-singh-05 <141124568+akash-singh-05@users.noreply.github.com>
2024-09-20 16:07:28 +0530

    Add files via upload

---
+++ Python_Day19_Numpy Stastical Function_Assigment1.py
@@ -0,0 +1,10 @@
+'''Compute the median of the flattened NumPy array 
+Input: x_odd = np.array([1, 2, 3, 4, 5, 6, 7])'''
+
+import numpy as np
+
+x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+median_value = np.median(x_odd)
+print(median_value)
+
+#Output:- 4.0
---
+++ Python_Day19_Numpy Stastical Function_Assigment2.py
@@ -0,0 +1,10 @@
+'''Compute the standard deviation of the NumPy array
+ Input: arr = [20, 2, 7, 1, 34]'''
+
+import numpy as np
+
+arr = [20, 2, 7, 1, 34]
+std_dev = np.std(arr)
+print(std_dev)
+
+#Output:- 12.576167937809991
