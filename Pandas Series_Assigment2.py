4ba6cc2c03386643ce3f0cf0ea5f2648fe8e1ff3
akash-singh-05 <141124568+akash-singh-05@users.noreply.github.com>
2024-10-03 15:46:44 +0530

    Add files via upload

---
+++ Pandas Series_Assigment2.py
@@ -0,0 +1,21 @@
+'''Suppose you want to track and analyze your household expenses for a month. You have recorded the expenses for various categories,
+such as groceries, utilities, rent, transportation, and entertainment. You can represent this expense data using a Pandas Series.'''
+
+import pandas as pd
+
+# Input data
+categories = ['Groceries', 'Utilities', 'Rent', 'Transportation', 'Entertainment']
+expenses = [500, 200, 1200, 300, 150]
+
+# Create a Pandas Series
+expenses_series = pd.Series(expenses, index=categories)
+
+# Display the Series
+print(expenses_series)
+
+'''Output:- Groceries          500
+            Utilities          200
+            Rent              1200
+            Transportation     300
+            Entertainment      150
+            dtype: int64 '''
